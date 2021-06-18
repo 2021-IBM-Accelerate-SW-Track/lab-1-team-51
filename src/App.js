2b03c906e4cdd09892b7dda54d4bc9b6dad27733
Terrance <Tbarnett2020@gmail.com>
2021-06-17 22:52:48 -0400

    ToDo List Team1
    Todo can be added
    Todo pops up when OnSubmit
    Todo has delete feature (can be redone)
    Let me know if its okay.
    
    Co-Authored-By: riveramatthew <61847038+riveramatthew@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import Header from "./component/header"
+import React, {useState} from 'react';
 import './App.css';
+// importing components up here
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  // Input is the state & SetInput is the function that allows you to change that value
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-    <Header/>
+      <header>
+      <h1>51's To-Do list</h1>
+      </header>
+      {/* Using Form below almost like an HTML element 
+      adding setInputText from above gives us access to it in the Form component*/}
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      {/* Using Todo below almost like an HTML element */}
+      <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
 
 export default App;
+
+
+// Will hook up components to main App.js

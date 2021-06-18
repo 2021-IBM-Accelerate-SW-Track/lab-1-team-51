43a6c90d3d5aebb6ec8ccaebade68394d45f44f9
adybabe1 <77403245+adybabe1@users.noreply.github.com>
2021-06-18 15:58:29 -0400

    for date and time

--- src/App.js
+++ src/App.js
@@ -3,11 +3,59 @@ import './App.css';
 // importing components up here
 import Form from './components/Form';
 import TodoList from './components/TodoList';
+// for the date and time here
+import DateFnsUtils from '@date-io/date-fns'
+import{
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboarddatePicker
+}from '@material-ui/pickers'
 
 function App() {
   // Input is the state & SetInput is the function that allows you to change that value
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  
+  const[selectedDate, serSelectedDate] = React.useState(
+  new Date("2021-06-18f12:00:00")
+)
+
+  const handleDateChange = (date) =>{
+     setSelectedDate(date)
+}
+  // Input is the state & SetInput is the function that allows you to change that value
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="App">
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+    <Grid container justify='space-around'>
+    <KeyboarddatePicker
+    disableToolbar
+    variant='inline'
+    format='MM/dd/yyyy'
+    margin='normal'
+    id='date-picker'
+    label='Date Picker'
+    value={selectedDate}
+    onChange={handleDateChange}
+    KeyboardButtonProps={{
+      'aria-label': 'cahnge-date'
+    }}
+    />
+    <KeyboardTimePicker
+    margin='normal'
+    id='time-picker'
+    label='Time Picker'
+    value={selectedDate}
+    onChange={handleDateChange}
+    KeyboardButtonProps={{
+      'aria-label': 'cahnge-date'
+    }}
+    />
+
+    </Grid>
+    </MuiPickersUtilsProvider>
   return (
     <div className="App">
       <header>
